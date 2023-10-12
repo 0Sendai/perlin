87e37f646ddab3ca003366d60706842eae64ed40
Preshepka <147092374+Preshepka@users.noreply.github.com>
2023-10-12 20:12:12 +0300

    documentation correction test_perlin.py

--- test_perlin.py
+++ test_perlin.py
@@ -22,12 +22,7 @@ def shuffle(array):
 
 def makeperm():
     """This code implements the makeperm function,
-       which creates and returns a permutation list.
-       It first creates a list of numbers from 0 to 255,
-       then shuffles it randomly using the shuffle function,
-       and finally adds duplicate items to the end of the list.
-       The result will be a list containing randomly shuffled numbers
-       from 0 to 255."""
+       which creates and returns a permutation list."""
     perm = []
     for i in range(0,256):
         perm.append(i)
@@ -40,9 +35,7 @@ perm = makeperm()
 
 def get_random_gradient(v):
     """Depending on the value of v,
-       it selects one of four possible gradients:
-       (1.0, 1.0), (1.0, -1.0), (-1.0, 1.0), or (-1.0, -1.0)
-       and returns it."""
+       it selects one of four possible gradients and returns it."""
     h = v & 3
     if h == 0:
         res = vector(1.0,1.0)
@@ -57,11 +50,7 @@ def get_random_gradient(v):
 
 
 def dist_grad_prod(x0, y0, x, y,v):
-    """This code implements the function dist_grad_prod,
-       which calculates the dot product between the random
-       gradient and a vector representing the difference in coordinates
-       between two points (x0, y0) and (x, y).
-       The result of this product is returned from the function."""
+    """The function calculates the dot product between the random gradient and the vector"""
     gradient = get_random_gradient(v)
 
     dx = x0 - x
@@ -71,17 +60,11 @@ def dist_grad_prod(x0, y0, x, y,v):
 
 
 def interpolate(d0,d1,weight):
-    """This code implements the interpolate function,
-       which calculates the interpolated value between
-       d0 and d1 using the weight factor."""
+    """This code implements the interpolate function."""
     return (d1-d0) * (3.0 - weight * 2.0) * weight * weight + d0
 
 def perlin(x,y):
-    """This code implements a perlin function that is used to
-        calculate noise in two-dimensional (x, y) coordinates.
-        It calculates the integer coordinates of cells (x0, y0)
-        as well as the coordinates of neighboring cells (x1, y1).
-        These values are used for further noise calculations."""
+    """This function is used to calculate perlin noise in two-dimensional (x, y) coordinates."""
     x0 = int(x)
     y0 = int(y)
     x1 = x0 + 1
